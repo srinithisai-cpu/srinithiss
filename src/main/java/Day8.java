47d899f3167a07da1c5dbd23e93712c6dbfc8183
srinithisai-cpu <srinithis25ai@vetias.ac.in>
2026-06-13 12:37:03 +0530

    Day8 added

---
+++ src/main/java/Day8.java
@@ -0,0 +1,12 @@
+public class Day8 {
+    record CourseRecord(String name, String dept) {
+
+    }
+
+   void main() {
+         CourseRecord courserecord = new CourseRecord("java programming", "artificial intelligence");
+        System.out.println(courserecord.name());
+        System.out.println(courserecord.dept());
+
+    }
+}
