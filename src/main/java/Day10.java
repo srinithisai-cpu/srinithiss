f03ab4c06ea9cdabd753084bc0ed20aef337a84b
srinithiss <srinithisai@vetias.ac.in>
2026-06-17 09:43:06 +0530

    added file Day10

---
+++ src/main/java/Day10.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+    public class Day10{
+        public static void main(){
+            ArrayList<String> students = new ArrayList<>();
+            students.add("Abi");
+            students.add("Raju");
+            students.add("Rani");
+            students.add("Ram");
+            students.add("Rama" );
+            System.out.println("Student List:");
+            for (String s : students) {
+                System.out.println(s);
+            }
+            students.remove("Ram");
+            System.out.println("\nAfter Removing Ram:");
+            for (String s : students) {
+                System.out.println(s);
+            }
+        }
+    }
+
+
+
+import java.util.LinkedList;
+public class Day10 {
+    static void process(LinkedList<String> queue) {
+        System.out.println("Removed:" + queue.removeFirst());
+    }
+
+    static void main() {
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        queue.add("E");
+
+        while (queue.size() > 1) {
+            process(queue);
+        }
+    }
+}
+static void process(LinkedList<String>List) {
+    System.out.println("removed:" + List.removeFirst());
+}
+static void main() {
+    LinkedList<String> list = new LinkedList<>();
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.add("d");
+    list.add("e");
+    while (list.size() > 1) {
+        process(list);
+    }
+}
+
+
+
