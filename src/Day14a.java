f80501ef45bff0451aafa8afeeeef220f5ef8fb9
srinithiss <srinithisai@vetias.ac.in>
2026-06-22 14:57:02 +0530

    Day14 added

---
+++ src/Day14a.java
@@ -0,0 +1,30 @@
+public class Day14a {
+    interface Notification {
+        void send(String to,String Message);
+    }
+    static class EmailNotification implements Notification {
+        @Override
+        public void send(String to,String Message){
+            System.out.println("sending Email:"+ Message + ",to" + to );
+        }
+    }
+     static class WhatsappNotification implements Notification{
+        @Override
+        public void send(String to,String Message){
+            System.out.println("sending WhatsApp:"+Message+",to"+to);
+        }
+    }
+    static Notification getNotification(Day14a factory , String type){
+        return switch (type){
+            case "Email"-> new EmailNotification();
+            case "WhatsApp"-> new WhatsappNotification();
+            default -> throw new IllegalArgumentException("Invaild Notification type:" +type);
+
+        };
+    }
+    static void main(){
+        Day14a factory = new Day14a();
+        String type = "WhatsApp";
+        getNotification(factory,type).send("1233456789","Hello world");
+    }
+}
